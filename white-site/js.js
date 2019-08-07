5114bb7266809716f9ee19ba59c7debcfe09323d
dima <dimavasylyna@gmail.com>
2019-08-07 19:31:03 +0300

    пошук імпортованих стилів успішно проходить. Пошук картинок в імортованих стилях не працює

--- white-site/js.js
+++ white-site/js.js
@@ -3,7 +3,7 @@
 
 // Файли, які необхідно просканувати на ресурси
 const htmlFilePathList = [
-	`index.html`,
+	`main/index.html`,
 	`uslugi/index.html`,
 	`contacts/contacts/contacts/index.html`
 ];
@@ -41,28 +41,52 @@ let getLinks = (selector, attr, file) => {
 }
 
 let parseCss = (filePath) => {
-	const bgUrlStyleRegex = /([:,\s]\s*url\s*\(\s*((?:'(\S*?)'|"(\S*?)"|((?:\\\s|\\\)|\\\"|\\\'|\S)*?))\s*)\))/gi;
-	// console.log(filePath + ` filePath ------------`);
-	let styleFile = fs.readFileSync(filePath, `utf8`);
-	let bgUrl = styleFile.match(bgUrlStyleRegex);
-	let bgUrlStyleObj = {};
+	// якщо файл з вказаним шляхом існує
+	if (fs.existsSync(filePath)) {
+		const bgUrlStyleRegex = /([:,\s]\s*url\s*\(\s*((?:'(\S*?)'|"(\S*?)"|((?:\\\s|\\\)|\\\"|\\\'|\S)*?))\s*)\))/gi;
+		// console.log(filePath + ` filePath ------------`);
+		let styleFile = fs.readFileSync(filePath, `utf8`);
+		let bgUrl = styleFile.match(bgUrlStyleRegex);
+		let bgUrlStyleObj = Object.create(null);
 
-// підчищаємо за регуляркою лапки
-	if (bgUrl) {
-		bgUrl.forEach((elem)=>{
-			elem = elem.replace(bgUrlStyleRegex, `$2`);
-			if (elem[elem.length - 1] === `'` || elem[elem.length - 1] === `"` ) {
-				elem = elem.substring(0, elem.length - 1);
-			}
-			if (elem[0] === `'` || elem[0] === `"`) {
-				elem = elem.substring(1);
-			}
-			let absPath = path.resolve(__dirname, `${path.dirname(filePath)}/`, elem);
-			bgUrlStyleObj[absPath] =  true;
-		});
+
+		// ------- пошук підключених стилів
+		let importStylesRegex = /@import[ ]*['\"]{0,}(url\()*['\"]*([^;'\"\)]*)['\"\)]*/gi;
+		let importStylesUrl = styleFile.match(importStylesRegex);
+		let importStyleUrlObj = Object.create(null);
+
+		if (importStylesUrl) {
+			importStylesUrl = importStylesUrl.forEach((importUrl)=>{
+				let url = importUrl.replace(importStylesRegex, `$2`);
+				let absPath = path.resolve(__dirname, `${path.dirname(`main/style.css`)}/`, url);
+				importStyleUrlObj[absPath] = true;
+				// якщо є стилі підключені через import, запускаємо для них ф-цію
+				// працює рекурсивно
+				parseCss(absPath);
+			});
+
+
+		}
+
+
+	// підчищаємо за регуляркою лапки
+		if (bgUrl) {
+			bgUrl.forEach((elem)=>{
+				elem = elem.replace(bgUrlStyleRegex, `$2`);
+				if (elem[elem.length - 1] === `'` || elem[elem.length - 1] === `"` ) {
+					elem = elem.substring(0, elem.length - 1);
+				}
+				if (elem[0] === `'` || elem[0] === `"`) {
+					elem = elem.substring(1);
+				}
+				let absPath = path.resolve(__dirname, `${path.dirname(filePath)}/`, elem);
+				bgUrlStyleObj[absPath] =  true;
+				// console.log(`${absPath}-----------------------------------********`);
+			});
+		}
+
+		return bgUrlStyleObj;
 	}
-	// console.log(bgUrlStyleObj);
-	return bgUrlStyleObj;
 }
 let findAllDependence = (file) => {
 	// знаходимо шляхи всіх ресурсів, які використовуються
@@ -70,11 +94,14 @@ let findAllDependence = (file) => {
 	let objScripts = getLinks(`script[src]`, `src`, file);
 	let objImg = getLinks(`img[src]`, `src`, file);
 	let favicons = getLinks(`link[rel*=icon]`, `href`, file);
-	let objUsedFilesPathInStyles = {};
+	let objUsedFilesPathInStyles = Object.create(null);
 	// парсимо стилі
     for (let styleFile in objStyles) {
         objUsedFilesPathInStyles = Object.assign(objUsedFilesPathInStyles, parseCss(styleFile));
     }
+	for (let key in objUsedFilesPathInStyles) {
+		console.log(key);
+	}
     // парсимо html
 	objUsedInlineStyles = parseCss(file);
 	console.log(objUsedInlineStyles);
@@ -131,7 +158,10 @@ let removeUnusedFiles = () => {
 	})
 	console.log(count + ` used`);
 }
-removeUnusedFiles();
+// removeUnusedFiles();
+
+
+
 // записуємо в файл
     // fs.writeFile("out.html", data,  function(err) {
     //     if(err) {
--- white-site/parseStyle.js
+++ white-site/parseStyle.js
@@ -3,9 +3,24 @@ const fs = require(`fs`);
 // модуль для роботи зі шляхами
 const path = require(`path`);
 const bgUrlStyleRegex = /([:,\s]\s*url\s*\(\s*((?:'(\S*?)'|"(\S*?)"|((?:\\\s|\\\)|\\\"|\\\'|\S)*?))\s*)\))/gi;
-let styleFile = fs.readFileSync(`bitrix/cache/css/s1/stomatolog/kernel_main/kernel_main_v10d98.css`, `utf8`);
+let styleFile = fs.readFileSync(`main/style.css`, `utf8`);
 let bgUrl = styleFile.match(bgUrlStyleRegex);
 let bgUrlStyleObj = {};
+
+
+// -------
+let importStylesRegex = /@import[ ]*['\"]{0,}(url\()*['\"]*([^;'\"\)]*)['\"\)]*/gi;
+let importStylesUrl = styleFile.match(importStylesRegex);
+
+if (importStylesUrl) {
+    importStylesUrl = importStylesUrl.map((importUrl)=>{
+         let url = importUrl.replace(importStylesRegex, `$2`);
+        let absPath = path.resolve(__dirname, `${path.dirname(`main/style.css`)}/`, url);
+        return absPath;
+    });
+    console.log(importStylesUrl);
+}
+
 
 // підчищаємо за регуляркою лапки
 bgUrl.forEach((elem)=>{
@@ -17,6 +32,7 @@ bgUrl.forEach((elem)=>{
         elem = elem.substring(1);
     }
 
-    bgUrlStyleObj[elem] =  true;
+    let absPath = path.resolve(__dirname, `${path.dirname(`main/style.css`)}/`, elem);
+    bgUrlStyleObj[absPath] =  true;
 });
 console.log(bgUrlStyleObj);
